480f9b431aa2ce044e436e494ef81a1cc3a9a560
Douglas254 <douglasobara97@gmail.com>
2020-08-12 20:33:17 +0300

    fist app in django

--- DEMOPROJECT/DEMOPROJECT/settings.py
+++ DEMOPROJECT/DEMOPROJECT/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'webapp',
+    #My added apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
