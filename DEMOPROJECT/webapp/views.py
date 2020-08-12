6c892f4c78d0fc637da8399afad141c14aabccf4
Douglas254 <douglasobara97@gmail.com>
2020-08-12 21:16:49 +0300

    url

--- DEMOPROJECT/webapp/views.py
+++ DEMOPROJECT/webapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def index(request):
+def hi(request):
     return HttpResponse("<h1>WELCOME</h1>")
