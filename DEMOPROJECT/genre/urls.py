935bfa3d4c276881561bd8c2e9e0f84ca58fa5b9
Douglas254 <douglasobara97@gmail.com>
2020-08-22 17:54:03 +0300

    update view.py

--- DEMOPROJECT/genre/urls.py
+++ DEMOPROJECT/genre/urls.py
@@ -2,4 +2,4 @@
 from django.conf.urls import url
 from . import views
 urlpatterns = [
-    url('', views.gen,name='genre-Page')]
+    url('', views.gen,name='genre-Page ')]
--- DEMOPROJECT/webapp/views.py
+++ DEMOPROJECT/webapp/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def hi(request):
-    return HttpResponse("<h1>WELCOME</h1>")
+    return HttpResponse("<h1>WELCOME</h1>"
+                        "<h2>This Python Django Website </h2>"
+                        "Keep going and <b><marquee>learn</marquee></b> new stuff")
