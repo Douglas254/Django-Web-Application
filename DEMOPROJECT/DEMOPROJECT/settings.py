d7239b3aa1658c1d8d0d6b945abb7eddbb2b605f
Douglas254 <douglasobara97@gmail.com>
2020-09-08 20:46:30 +0300

    render html file

--- DEMOPROJECT/DEMOPROJECT/settings.py
+++ DEMOPROJECT/DEMOPROJECT/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+TEMPLATE_DIR = BASE_DIR/'templates'
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -57,7 +59,7 @@ ROOT_URLCONF = 'DEMOPROJECT.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- DEMOPROJECT/webapp/views.py
+++ DEMOPROJECT/webapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+#from django.http import HttpResponse
+#Create your views here.
 def hi(request):
     return HttpResponse("<h1>WELCOME</h1>"
-                        "<h2>This Python Django Website </h2>"
-                        "Keep going and <b><marquee>learn</marquee></b> new stuff")
+                       "<h2>This Python Django Website </h2>"
+                        "Keep going and <b><i><marquee>learn</marquee></i></b> new stuff")
